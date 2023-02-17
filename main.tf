26df2b1b6b4adc9eeeefcb5c920a487ca0d18ece
Arman Dabiri <arman_dabiri@hotmail.com>
2023-02-17 05:01:23 +0330

    Name format changed

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Create a server
 resource "hcloud_server" "vpn-server" {
   count       = var.counter
-  name        = "${var.name}${var.counter}"
+  name        = "${var.name}-${var.counter}"
   image       = var.image
   server_type = var.server_type
   datacenter  = var.datacenter
